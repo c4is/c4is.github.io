a38d7f4303682d7ba534ee3e804a08af6dd712f6
c4is <87481985+c4is@users.noreply.github.com>
2021-07-24 07:14:26 +0000

    Update and rename cb.php to telnyx.php

--- telnyx.php
+++ telnyx.php
@@ -2,5 +2,5 @@
 $to = "admin@c4is.army ";
 $subject = "Text Message from {$_REQUEST['From']} to {$_REQUEST['To']}";
 $message = "{$_REQUEST['Body']}";
-$headers = "From: telnyx@yourdomain.com ";
+$headers = "From: telnyx@c4is.army ";
 mail($to, $subject, $message, $headers);
